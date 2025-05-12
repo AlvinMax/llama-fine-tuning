e08ed199f7730fc92bc1ce32235d91a113ffdc0f
Maksim.Melekhovets <maksim.melekhovets@jetbrains.com>
2025-05-13 00:34:36 +0200

    actualize the fine-tuning example

--- run.py
+++ run.py
@@ -2,10 +2,9 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import LoraConfig
 from datasets import load_dataset
-from transformers import TrainingArguments
-from trl import SFTTrainer
+from trl import SFTTrainer, SFTConfig
 
-# Load the 7b mistral model
+# Load the Mistral-7B model
 model_id = "mistralai/Mistral-7B-v0.1"
 
 quantization_config = BitsAndBytesConfig(
@@ -14,13 +13,12 @@ quantization_config = BitsAndBytesConfig(
     bnb_4bit_quant_type="nf4"
 )
 
-# Load model
+# Load model with quantization
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config)
 
 # Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-# Set it to a new token to correctly attend to EOS tokens.
-tokenizer.add_special_tokens({'pad_token': '<PAD>'})
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
 
 lora_config = LoraConfig(
     r=8,
@@ -29,41 +27,40 @@ lora_config = LoraConfig(
     task_type="CAUSAL_LM",
 )
 
+# Apply LoRA adapter
 model.add_adapter(lora_config)
 
+# Load dataset
 train_dataset = load_dataset("stingning/ultrachat", split="train[:1%]")
-# noinspection PyTypeChecker
-training_arguments = TrainingArguments(
-    output_dir="/tmp/llama-7b-qlora-ultrachat",
+
+# Training configuration
+training_arguments = SFTConfig(
+    output_dir="./outputs",
     per_device_train_batch_size=4,
-    gradient_accumulation_steps=4,
-    optim="paged_adamw_32bit",
-    save_steps=10,
+    max_steps=10,
     logging_steps=1,
-    learning_rate=2e-4,
-    max_grad_norm=0.3,
-    max_steps=30,
-    warmup_ratio=0.03,
-    lr_scheduler_type="constant",
-    gradient_checkpointing=True,
+    learning_rate=1e-4,
+    max_length=512,
+    packing=True,
     report_to="none",  # report_to="wandb"
 )
 
 
 def formatting_func(example):
-    text = f"### USER: {example['data'][0]}\n### ASSISTANT: {example['data'][1]}"
+    text = ""
+    for idx, msg in enumerate(example["data"]):
+        if idx % 2 == 0:
+            text += f"<|user|>\n{msg}{tokenizer.eos_token}\n"
+        else:
+            text += f"<|assistant|>\n{msg}{tokenizer.eos_token}\n"
     return text
 
 
-# noinspection PyTypeChecker
 trainer = SFTTrainer(
     model=model,
     args=training_arguments,
     train_dataset=train_dataset,
-    packing=True,
-    dataset_text_field="id",
-    tokenizer=tokenizer,
-    max_seq_length=1024,
+    processing_class=tokenizer,
     formatting_func=formatting_func,
 )
 trainer.train()
